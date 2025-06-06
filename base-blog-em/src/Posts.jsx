c56fe9b03cae3e0d5028413b14803aba7dbed06b
Robin <philoin88@gmail.com>
2025-06-06 21:58:26 +0900

    feat: Basic concept for Tanstack Query with Blog Site

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import { fetchPosts, deletePost, updatePost } from './api';
 import { PostDetail } from './PostDetail';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const queryClient = useQueryClient();
+
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  });
+
+  useEffect(() => {
+    if (currentPage >= maxPostPage) return;
+
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery({
+      queryKey: ['posts', nextPage],
+      queryFn: () => fetchPosts(nextPage),
+    });
+  }, [currentPage, queryClient]);
+
+  const { data, error, isLoading, isFetching, isError } = useQuery({
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 5000,
+  });
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isFetching) return <h3>Fetching...</h3>;
+  if (isError)
+    return (
+      <h3>
+        Oops, something went wrong <br />
+        <p>{error.toString()}</p>
+      </h3>
+    );
 
   return (
     <>
@@ -18,23 +51,41 @@ export function Posts() {
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              updateMutation.reset();
+              deleteMutation.reset();
+              setSelectedPost(post);
+            }}
           >
             {post.title}
           </li>
         ))}
       </ul>
+
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+        >
           Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          updateMutation={updateMutation}
+          deleteMutation={deleteMutation}
+        />
+      )}
     </>
   );
 }
