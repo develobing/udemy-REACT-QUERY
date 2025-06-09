2881bb875fbe7521c2fad18a7aa611fc24e1b76a
Robin <philoin88@gmail.com>
2025-06-09 09:19:22 +0900

    feat: Add Mutation on Appointment

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,8 +1,16 @@
-import { QueryCache, QueryClient } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 
 import { toast } from '@/components/app/toast';
 
-function errorHandler(errorMsg: string) {
+function createTitle(errorMsg: string, actionType: 'query' | 'mutation') {
+  const action = actionType === 'query' ? 'fetch' : 'update';
+  const title = `could not ${action} data: ${
+    errorMsg ?? 'error connecting to server'
+  }`;
+  return title;
+}
+
+function errorHandler(title: string) {
   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
   // one message per page load, not one message per query
   // the user doesn't care that there were three failed queries on the staff page
@@ -10,10 +18,6 @@ function errorHandler(errorMsg: string) {
   const id = 'react-query-toast';
 
   if (!toast.isActive(id)) {
-    const action = 'fetch';
-    const title = `could not ${action} data: ${
-      errorMsg ?? 'error connecting to server'
-    }`;
     toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
   }
 }
@@ -34,7 +38,15 @@ export const queryClient = new QueryClient({
 
   queryCache: new QueryCache({
     onError: (error) => {
-      errorHandler(error.message);
+      const title = createTitle(error.message, 'query');
+      errorHandler(title);
+    },
+  }),
+
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      const title = createTitle(error.message, 'mutation');
+      errorHandler(title);
     },
   }),
 });
