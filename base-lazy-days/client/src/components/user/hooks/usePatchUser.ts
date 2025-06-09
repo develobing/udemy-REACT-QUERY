7f746d25bb41cec91ee9ba30c7a1c943fa91e7aa
Robin <philoin88@gmail.com>
2025-06-10 00:09:52 +0900

    feat: Implement Optimistic Update on User Profile

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import jsonpatch from 'fast-json-patch';
 
 import type { User } from '@shared/types';
@@ -7,6 +7,9 @@ import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { useUser } from './useUser';
 
 import { toast } from '@/components/app/toast';
+import { queryKeys } from '@/react-query/constants';
+
+export const MUTATION_KEY = 'patch-user';
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -29,13 +32,20 @@ async function patchUserOnServer(
 }
 
 export function usePatchUser() {
-  const { user, updateUser } = useUser();
+  const queryClient = useQueryClient();
+  const { user } = useUser();
 
   const { mutate: patchUser } = useMutation({
+    mutationKey: [MUTATION_KEY],
     mutationFn: (newData: User) => patchUserOnServer(newData, user),
-    onSuccess: (data: User | null) => {
+    onSuccess: () => {
       toast({ title: 'User updated successfully!', status: 'success' });
-      updateUser(data);
+    },
+    onSettled: () => {
+      // return promise to maintain 'inProgress' status until the query is invalidated
+      return queryClient.invalidateQueries({
+        queryKey: [queryKeys.user],
+      });
     },
   });
 
